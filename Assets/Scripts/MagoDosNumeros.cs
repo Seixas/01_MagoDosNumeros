4c2139aa1d239bde86cbefe459149ddaf6f39560
Lucas Seixas <seixas.lc@gmail.com>
2017-06-01 11:19:55 -0300

    MN002 - Refactory, cleancode com funções e saida da aplicação
    Adicionado a opção para sair do jogo ou continuar

--- Assets/Scripts/MagoDosNumeros.cs
+++ Assets/Scripts/MagoDosNumeros.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MagoDosNumeros : MonoBehaviour {
-	public int max = 2000;
-	public int min = 1;
-	int supor = 1000;
+	int max;
+	int min;
+	int supor;
 
 	// Use this for initialization
 	void Start () {
+		StartGame ();
+	}
+
+	void StartGame() {
+		max = 2000;
+		min = 1;
+		supor = 1000;
 		max = max + 1;
+
+		print ("=============================");
 		print ("Bem vindo ao mago dos numeros");
 		print ("Pense em um numero");
 
@@ -18,24 +27,32 @@ public class MagoDosNumeros : MonoBehaviour {
 
 		print ("Esse numero e maior ou menor que " + supor + "?");
 		print ("Cima = maior, Baixo = menor, Enter = igual");
-
 	}
-	
+		
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.UpArrow)) {
 			//print ("Seta de cima pressionada");
 			min = supor;
-			supor = (max + min) / 2;
-			print ("Maior ou menor que " + supor + "?");
+			ProximoSupor();
 		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
 			//print ("Seta de baixo pressionada");
 			max = supor;
-			supor = (max + min) / 2;
-			print ("Maior ou menor que " + supor + "?");
+			ProximoSupor();
 		} else if (Input.GetKeyDown(KeyCode.Return)) {
 			print ("O MAGO E IMPLACAVEL!");
 			print ("Deseja tentar de novo?");
+			if (Input.GetKey("escape")) {
+				Application.Quit();
+			} else {
+				StartGame();
+			}
 		}
+	}
+
+	void ProximoSupor() {
+		supor = (max + min) / 2;
+		print ("Maior ou menor que " + supor + "?");
+		print ("Cima = maior, Baixo = menor, Enter = igual");
 	}
 }
